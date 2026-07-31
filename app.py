dd3c1c908fc4a3eb88c86120d0d15b47464a75ab
Stéphane Sadowski <stephane.sadowski@cerema.fr>
2026-07-31 16:15:32 +0200

    deactivate setlocale

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import locale
 
-locale.setlocale(locale.LC_ALL, "fr-FR.UTF-8")
+#locale.setlocale(locale.LC_ALL, "fr-FR.UTF-8")
 
 # =============================================================
 # FONCTIONS
